20f19fa43baf3503d8588a343cc6085d1979b3bd
gjaidev <jaidevgilla1995@gmail.com>
2022-04-20 11:51:38 -0500

    Added account role

--- src/main/java/com/collegeProject/controller/OnlineLearningController.java
+++ src/main/java/com/collegeProject/controller/OnlineLearningController.java
@@ -5,6 +5,7 @@ import com.collegeProject.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class OnlineLearningController {
     @Autowired
